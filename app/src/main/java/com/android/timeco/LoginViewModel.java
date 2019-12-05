8763868cecab8a7f8dc6d3a705f671369ca79c48
AndresLQ1987 <andreslq1987.alq@gmail.com>
2019-12-05 14:27:17 -0800

    Add Login Fragment whit View and ViewModel

---
+++ app/src/main/java/com/android/timeco/LoginViewModel.java
@@ -0,0 +1,10 @@
+package com.android.timeco;
+
+import androidx.lifecycle.ViewModel;
+
+/**
+ * Class LoginViewModel have methods to process the information gets in view login
+ */
+public class LoginViewModel extends ViewModel {
+    //TODO methods doLogin, checkUser, checkPassword
+}
