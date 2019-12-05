2602b9ac9f691d827dcf9eeae1356e5e0e2bd530
AndresLQ1987 <andreslq1987.alq@gmail.com>
2019-12-05 15:16:51 -0800

    add to MainActivity draw the first fragment LoginFragment

--- app/src/main/java/com/android/timeco/MainActivity.java
+++ app/src/main/java/com/android/timeco/MainActivity.java
@@ -1,14 +1,27 @@
 package com.android.timeco;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
+import com.android.timeco.View.LoginFragment;
+
+/**
+ * Main Activity where the fragments are drawn
+ */
 public class MainActivity extends AppCompatActivity {
+
+    FragmentManager fm;
+    AccessData accessData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Draw the first fragment
+        fm = getSupportFragmentManager();
+        fm.beginTransaction().replace(R.id.MainActivity, new LoginFragment()).commit();
     }
 }
