30c3d5199cd7927425141dfec0aaa37f10d13003
AndresLQ1987 <andreslq1987.alq@gmail.com>
2019-12-05 20:08:23 -0800

    Add Fragment,View,ViewModel of staff management

---
+++ app/src/main/java/com/android/timeco/ViewModel/StaffViewModel.java
@@ -0,0 +1,10 @@
+package com.android.timeco.ViewModel;
+
+import androidx.lifecycle.ViewModel;
+
+/**
+ * Class StaffViewModel have methods to process the information gets in view staff
+ */
+public class StaffViewModel extends ViewModel {
+    //TODO Methods to process staff add or remove workers, edit, etc.
+}
