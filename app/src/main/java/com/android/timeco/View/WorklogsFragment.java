1e9afd7052280851ebbe5acbacbb9abdbee37b1e
AndresLQ1987 <andreslq1987.alq@gmail.com>
2019-12-05 21:56:12 -0800

    Add Worklogs Fragment, View, ViewModel

---
+++ app/src/main/java/com/android/timeco/View/WorklogsFragment.java
@@ -0,0 +1,69 @@
+package com.android.timeco.View;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.timeco.R;
+
+public class WorklogsFragment extends Fragment {
+
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    /**
+     * Elements of Fragment
+     */
+
+
+    public WorklogsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment WorklogsFragment.
+     */
+    public static WorklogsFragment newInstance(String param1, String param2) {
+        WorklogsFragment fragment = new WorklogsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View ui_layout = inflater.inflate(R.layout.fragment_worklogs, container, false);
+
+        //TODO Methods gets information to ViewModel
+
+        return ui_layout;
+    }
+
+    //TODO Recycler View
+}
