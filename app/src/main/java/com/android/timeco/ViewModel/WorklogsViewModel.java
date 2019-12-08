891425af60e7beaf6f43fd6a94c843a00a8fec73
AndresLQ1987 <andreslq1987.alq@gmail.com>
2019-12-08 13:16:51 -0800

    Adding to WorklogsFragment the ReciclerView and internal Class RecyclerAdapterWorklogs WIP , renamed id of recyclerview in fragment_worklogs, add worklogholder, WorklogsViewModel add function getWorklogs WIP

--- app/src/main/java/com/android/timeco/ViewModel/WorklogsViewModel.java
+++ app/src/main/java/com/android/timeco/ViewModel/WorklogsViewModel.java
@@ -2,9 +2,28 @@ package com.android.timeco.ViewModel;
 
 import androidx.lifecycle.ViewModel;
 
+import com.android.timeco.Model.Worklog;
+
+import java.util.ArrayList;
+
 /**
  * Class LoginViewModel have methods to process the information gets in view login
  */
 public class WorklogsViewModel extends ViewModel {
     //TODO Methods for view the work logs
+
+    /**
+     * Method for get the work logs of user
+     * @return
+     */
+    public ArrayList<Worklog> getWorklogs() {
+        //TODO change the code for get array of work logs from the user pass in arguments
+
+        //Temporal Array of work logs for test Recycler View
+        ArrayList<Worklog> listWorklogs = new ArrayList<>();
+
+        Worklog wl1 = new Worklog();
+
+        return listWorklogs;
+    }
 }
