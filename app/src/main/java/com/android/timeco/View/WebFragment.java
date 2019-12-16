f24aa4a39551ef282ddc3eb9027587e21cccfe49
ChristianVillegas <christian.v.garcia@gmail.com>
2019-12-16 20:24:56 +0100

    Thread Code on ViewWebFragment WP!

--- app/src/main/java/com/android/timeco/View/WebFragment.java
+++ app/src/main/java/com/android/timeco/View/WebFragment.java
@@ -1,7 +1,6 @@
 package com.android.timeco.View;
 
-import android.content.Context;
-import android.net.Uri;
+
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,29 +8,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.android.timeco.R;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link WebFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link WebFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class WebFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
+
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
+
+    /**
+     * Elements of Fragment
+     */
+
+    private Button button_cargar;
+    private TextView tv_title;
 
     public WebFragment() {
         // Required empty public constructor
@@ -45,7 +44,8 @@ public class WebFragment extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment WebFragment.
      */
-    // TODO: Rename and change types and number of parameters
+
+
     public static WebFragment newInstance(String param1, String param2) {
         WebFragment fragment = new WebFragment();
         Bundle args = new Bundle();
@@ -64,49 +64,32 @@ public class WebFragment extends Fragment {
         }
     }
 
+    /**
+     * Method to get interactions and throw information to View Model
+     *
+     * @param inflater inflater
+     * @param container container
+     * @param savedInstanceState saveInstanceState
+     * @return View
+     */
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_web, container, false);
-    }
+        View ui_layout = inflater.inflate(R.layout.fragment_web, container, false);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+        button_cargar = ui_layout.findViewById(R.id.button_cargar);
+        tv_title = ui_layout.findViewById(R.id.tv_title);
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
+        button_cargar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(
+                        R.id.MainActivity, new ViewWebFragment()).commit();
+            }
+        });
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        return ui_layout;
     }
 }
