1d5a32ee0a12773c79514a3e8200181dfce959e5
cf18andres.lopez <andreslq1987.alq@gmail.com>
2020-02-06 19:44:32 +0100

    Add conectionPostgress class whit thread

---
+++ app/src/main/java/com/android/timeco/ConectionPostgress.java
@@ -0,0 +1,13 @@
+package com.android.timeco;
+
+public class ConectionPostgress extends Thread {
+
+    @Override
+    public void run(){
+        try{
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
